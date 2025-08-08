4b6d9a5573d30a492744645daaed22c29e9248a4
McSwaggens <McSwaggens@protonmail.com>
2025-08-08 19:33:28 +1000

    Ir changes

--- ir.h
+++ ir.h
@@ -12,14 +12,6 @@ namespace IR {
 		u32 handle;
 	};
 
-	// struct Value {
-	// 	union {
-	// 		u64 i;
-	// 		Float32 fp32;
-	// 		Float64 fp64;
-	// 	};
-	// };
-
 	static LargeValue* CreateLargeValue(u64 bitcount) {
 	}
 
